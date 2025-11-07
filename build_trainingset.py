8e34d0c5cac3d5de406ec70b2cfd62ae9d9df999
Tomas McGuinness <tomas@tomasmcguinness.com>
2025-11-07 14:17:49 +0000

    Switch to flow temp and flow rate. Improve the tranining data.

--- build_trainingset.py
+++ build_trainingset.py
@@ -4,36 +4,92 @@ endpoint = "https://heatpumpmonitor.org/system/list/public.json"
 
 with urllib.request.urlopen(endpoint) as url:
     data = json.loads(url.read().decode())
-    #print(data)
+    # print(data)
 
-vaillant_systems_dict = [x for x in data if x['hp_manufacturer'] == 'Vaillant' 
-                        and x['hp_model'] == 'Arotherm+' 
-                        and x['hp_output'] == 5]
+# Take only Vaillant Arotherm+ 5kW systems
+#
+vaillant_systems_dict = [
+    x
+    for x in data
+    if x["hp_manufacturer"] == "Vaillant"
+    and x["hp_model"] == "Arotherm+"
+    and x["hp_output"] == 5
+]
 
-print(len(vaillant_systems_dict))
+with open("trainingset.csv", "w", newline="") as csvfile:
+    writer = csv.writer(
+        csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+    )
 
-with open('trainingset.csv', 'w', newline='') as csvfile:
+    writer.writerow(["Input", "FlowTemp", "FlowRate", "OutsideTemp"])
 
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    rowCount = 0
 
-    writer.writerow(['Input', 'TargetTemperature', 'OutsideTemperature'])
+    for system in vaillant_systems_dict:  # [:1]:
+        feeds_endpoint = "https://heatpumpmonitor.org/timeseries/available?id={id}"
+        time_series_endpoint = "https://heatpumpmonitor.org/timeseries/data?id={id}&start={start}&end={end}&feeds=heatpump_elec,heatpump_outsideT,heatpump_flowrate,heatpump_flowT&interval=3600&average=1&timeformat=notime"
 
-    for system in vaillant_systems_dict: #[:1]:
-
-        time_series_endpoint = "https://heatpumpmonitor.org/timeseries/data?id={id}&start=22-01-2023&end=06-09-2025&feeds=heatpump_elec,heatpump_roomT,heatpump_outsideT&interval=1800&average=1&timeformat=notime"
+        print("Processing system ID:", system["id"])
 
         try:
-            with urllib.request.urlopen(time_series_endpoint.format(id = system['id'])) as url:
-                time_series_data = json.loads(url.read().decode())
+            with urllib.request.urlopen(feeds_endpoint.format(id=system["id"])) as url:
+                feeds_data = json.loads(url.read().decode())
 
-                for i in range(len(time_series_data['heatpump_elec'])):
+                # Use the heatpump_elec feed to determine the time range.
+                #
+                start = feeds_data["feeds"]["heatpump_elec"]["start_time"]
+                end = feeds_data["feeds"]["heatpump_elec"]["end_time"]
 
-                    elec = time_series_data['heatpump_elec'][i]
-                    roomT = time_series_data['heatpump_roomT'][i]
-                    outsideT = time_series_data['heatpump_outsideT'][i]
+                # If this system doesn't have flow rate data, skip it
+                #
+                if "heatpump_flowrate" not in feeds_data["feeds"]:
+                    continue
 
-                    if elec is not None and roomT is not None and outsideT is not None:
-                        writer.writerow([elec, roomT, outsideT])    
+                flowrate_start = feeds_data["feeds"]["heatpump_flowrate"]["start_time"]
+                flowrate_end = feeds_data["feeds"]["heatpump_flowrate"]["end_time"]
+
+                # Ensure the flow rate data covers the same time period as the electricity data
+                if start != flowrate_start or end != flowrate_end:
+                    continue
+
+                # The unit of flow rate is optional and will vary between systems
+                #
+                if "unit" not in feeds_data["feeds"]["heatpump_flowrate"]:
+                    continue
+
+                flow_rate_unit = feeds_data["feeds"]["heatpump_flowrate"]["unit"]
+
+                with urllib.request.urlopen(
+                    time_series_endpoint.format(id=system["id"], start=start, end=end)
+                ) as url:
+                    time_series_data = json.loads(url.read().decode())
+
+                    for i in range(len(time_series_data["heatpump_elec"])):
+                        elec = time_series_data["heatpump_elec"][i]
+                        flowT = time_series_data["heatpump_flowT"][i]
+                        flowRate = time_series_data["heatpump_flowrate"][i]
+                        outsideT = time_series_data["heatpump_outsideT"][i]
+
+                        if (
+                            elec is not None
+                            and flowT is not None
+                            and flowRate is not None
+                            and outsideT is not None
+                        ):
+                            if flowRate < 5:  # skip very low flow rates
+                                continue
+
+                            if flow_rate_unit == "m³/h":  # Otherwise assume l/min
+                                flowRate = flowRate * 1000 / 60  # convert to l/min
+
+                            writer.writerow([elec, flowT, flowRate, outsideT])
+                            rowCount += 1
 
         except Exception as e:
-            print("An exception occurred: %s", repr(e))
+            print(
+                "An exception occurred processing system %d : %s. Skipping to the next system.",
+                system["id"],
+                repr(e),
+            )
+
+    print("Wrote ", rowCount)
--- train_and_predict.py
+++ train_and_predict.py
@@ -4,21 +4,21 @@ from sklearn import linear_model
 
 df = pandas.read_csv("trainingset.csv")
 
-X = df[['TargetTemperature', 'OutsideTemperature']]
+X = df[['FlowTemp', 'FlowRate', 'OutsideTemp']]
 y = df['Input']
 
 model = linear_model.LinearRegression()
 model.fit(X, y)
 
 # Save the model coefficients to a JSON file
-# coefficients = {
-#     'coef': model.coef_.tolist(),
-#     'intercept': model.intercept_.tolist()
-# }
+coefficients = {
+    'coef': model.coef_.tolist(),
+    'intercept': model.intercept_.tolist()
+}
 
-# with open('model_params.json', 'w') as f:
-#     json.dump(coefficients, f)
+with open('model_params.json', 'w') as f:
+    json.dump(coefficients, f)
 
-predictedInput = model.predict([[21, 10]])
+predictedInput = model.predict([[32, 14, 8]])
 
 print(predictedInput)
